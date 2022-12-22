39893b676bf65cf45030bcc4c33bc8e87018c8df
Mickaël Boussard <mickael.boussard@orange.fr>
2022-12-22 16:18:00 +0100

    test env variable

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 console.log('Hello World!');
 
-fetch('/.netlify/functions/config').then((response) => {
-  console.log(response);
-});
+// fetch('/.netlify/functions/config').then((response) => });
+
+const userAction = async () => {
+    const response = await fetch('/.netlify/functions/config');
+    const myJson = await response.json(); //extract JSON from the http response
+    console.log(myJson);
+    // do something with myJson
+  }
