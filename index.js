7c8b18b1313bf8afdaa5cb588b22605fe97432d4
Mickaël Boussard <mickael.boussard@orange.fr>
2022-12-22 16:15:03 +0100

    test env variable

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
 console.log('Hello World!');
 
-// const { VARIABLE_TEST } = process.env;
-
-console.log(`La valeur de la variable d'environnement est: ${VARIABLE_TEST}`);
+fetch('/.netlify/functions/config').then((response) => {
+  console.log(response);
+});
--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,7 +1,6 @@
-export function handler(event, context, callback) {
-    // Récupérer la valeur de la variable d'environnement
-    const variableTest = process.env.VARIABLE_TEST;
-  
-    // Utiliser la variable dans votre code
-    console.log(`La valeur de la variable d'environnement est: ${variableTest}`);
-  }
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World" }),
+  };
+};
