1392a67c772d4e9a8c149e61b53fcf4965c56441
Mickaël Boussard <mickael.boussard@orange.fr>
2022-12-22 16:31:06 +0100

    test env variable

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,10 +1,8 @@
-exports.handler = async (event, context) => {
-  // Retrieve the value of the VARIABLE_TEST environment variable
-  const variableTest = process.env.VARIABLE_TEST;
-
-  // Return the value of the environment variable in the response
+exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: "test",
+    // body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ test: "Hello World" }),
+
   };
 };
