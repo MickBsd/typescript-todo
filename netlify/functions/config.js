128f37c795de74f0fde333a2e6f27a2f3ac12be7
Mickaël Boussard <mickael.boussard@orange.fr>
2022-12-22 16:47:39 +0100

    test env variable

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,14 +1,18 @@
 exports.handler = async function (event, context) {
   const variableTest = process.env.VARIABLE_TEST;
+  let dataTest;
   fetch('https://dog.ceo/api/breeds/image/random')
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => dataTest = data);
   
   return {
-
-
-    // statusCode: 200,
-    // // body: JSON.stringify({ message: "Hello World" }),
-    body: JSON.stringify({ test: data }),
+    body: JSON.stringify({ test: data })
   };
+  // return {
+
+
+  //   // statusCode: 200,
+  //   // // body: JSON.stringify({ message: "Hello World" }),
+  //   body: JSON.stringify({ test: data }),
+  // };
 };
