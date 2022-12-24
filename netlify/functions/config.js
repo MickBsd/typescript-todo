ff60328638238b04735a79eb6eb2ba5eeab5c64e
Mickaël Boussard <mickael.boussard@orange.fr>
2022-12-24 16:05:43 +0100

    test env variable

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,29 +1,27 @@
-//import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
 export async function handler (event, context) {
   const variableTest = process.env.VARIABLE_TEST;
+  const url = "https://www.cityscan.fr/api/evaluations"
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ variableTest })
-  };
-  
-//   await fetch(getUrl, {
-//     method: "GET",
-//     headers: {
-//         "Content-Type": "application/json",
-//         "ApiKey": "Db}2]uWrY5TS@*Q"
-//     },
-// })
-// .then((response) => 
-//     response.json())
-// .then((data) => {
-//     console.log(data);
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({ data })
-//     };
-// });
-
-  
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify({ variableTest })
+  // };
+    
+  await fetch(getUrl, {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        "ApiKey": "Db}2]uWrY5TS@*Q"
+    },
+  })
+  .then((response) => 
+      response.json())
+  .then((data) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ data })
+      };
+  });
 }
